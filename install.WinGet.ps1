57cd32e2c42b2ae6ade6aaa6aa1d8d24773741fa
numbnet <zusyurec@gmail.com>
2020-12-13 23:20:11 +0200

    Subject line (try to keep under 50 characters)
    Multi-line description of commit, feel free to be detailed.
    
    [Ticket: X]
    
    	modified:   WinModules/OpenSSH/.gitignore

--- install.WinGet.ps1
+++ install.WinGet.ps1
@@ -14,3 +14,26 @@ Add-AppxPackage "C:\PS\WinGet.appxbundle"Invoke-WebRequest -Uri "https://github.
 ##  Install File
 Add-AppxPackage "C:\PS\WinGet.appxbundle"
 
+winget install Microsoft.VisualStudioCode --force
+winget install "Microsoft.WindowsTerminal" --silent
+winget install "Xampp" --silent
+winget install "CodeLite.CodeLite" --silent
+winget install "OpenJS.NodeJS" --silent
+ winget install "Nodist.Nodist" --silent
+ winget install "NuGet" --silent
+winget install "NuGetPackageExplorer.NuGetPackageExplorer" --silent
+winget search 'Microsoft.VC++'
+ winget install "Microsoft.PowerShell" --silent
+winget install "Microsoft.PowerShell-Preview" --silent
+
+
+
+Find-Module Posh-SSH | Install-Module
+Import-Module Posh-SSH
+Install-Module -Name Posh-SSH
+iex (New-Object Net.WebClient).DownloadString("https://gist.github.com/darkoperator/6152630/raw/c67de4f7cd780ba367cccbc2593f38d18ce6df89/instposhsshdev")
+
+
+
+
+echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA1kDD0kTWp0+m17hUY+FnDHE0YKdWJXCAuo4oiYyHsv zusyurec@gmail.com" | Out-File $env:USERPROFILE\.ssh\authorized_keys -Encoding ascii;
